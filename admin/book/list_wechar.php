007f9ed7b6aee4a1785cda8559c092f5e56f032f
Woo <siriussilen@hotmail.com>
2017-04-11 22:09:07 +0800

    add

--- admin/book/list_wechar.php
+++ admin/book/list_wechar.php
@@ -3,14 +3,14 @@ session_start();
 include ('../../conn.php');
 $conn = new conn("SET NAMES UTF8");
 $conn->execute_sql();
-$bname = $_POST['bname'];
-$sqls  = "SELECT ISBN AS bno,ISBN AS id,bname,bauthor AS author,binventory AS total,date as cdate,btype AS tname,bprice AS price,bnum AS remain FROM ls_book_all";
-$sql = "select count(*) as mum from ls_book_all"
-$conn->sql = $sqls." where bname like '%".$bname."%'";
-$res       = $conn->fetch_res();
+$bname      = $_POST['bname'];
+$sqls       = "SELECT ISBN AS bno,ISBN AS id,bname,bauthor AS author,binventory AS total,date as cdate,btype AS tname,bprice AS price,bnum AS remain FROM ls_book_all";
+$sql        = "select count(*) as mum from ls_book_all";
+$conn->sql  = $sql." where bname like '%".$bname."%'";
+$res        = $conn->fetch_res();
 $booksTotal = $res[0]['num'];
-$conn->sql = $sqls." where bname like '%".$bname."%'";
-$res       = $conn->fetch_res();
+$conn->sql  = $sqls." where bname like '%".$bname."%'";
+$res        = $conn->fetch_res();
 if (empty($res[0])) {
 	echo "{\"booksTotal\":".$booksTotal.",\"data\":[]}";
 } else {
